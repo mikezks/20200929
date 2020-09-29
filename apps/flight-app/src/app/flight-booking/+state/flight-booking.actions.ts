7170191a45819c7e5825adcfb1553427ed4a9e54
Michael Zikes <michael.zikes@intauria.com>
2020-09-29 16:37:57 +0200

    ngrx store

---
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -0,0 +1,18 @@
+import { Flight } from '@flight-workspace/flight-lib';
+import { createAction, props } from '@ngrx/store';
+
+export const flightsLoaded = createAction(
+  '[FlightBooking] Flights loaded',
+  props<{ flights: Flight[] }>()
+);
+
+/* export const loadFlightBookingsSuccess = createAction(
+  '[FlightBooking] Load FlightBookings Success',
+  props<{ data: any }>()
+);
+
+export const loadFlightBookingsFailure = createAction(
+  '[FlightBooking] Load FlightBookings Failure',
+  props<{ error: any }>()
+);
+ */
